6b35e9d0305b4ebd7cf0a2d185cff5764d21f86f
PetarPetrov89 <62606018+PetarPetrov89@users.noreply.github.com>
2020-04-05 16:03:14 +0300

    Exercises

---
+++ Exercises/TaksaTransport/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaksaTransport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете броя километри: ");
+            int km = int.Parse(Console.ReadLine());
+            Console.Write("Период на пътуване:");
+            string daytime = Console.ReadLine().ToString();
+
+
+            double taxiStart = 0.70;
+            double taxiDay = 0.79;
+            double taxiNight = 0.99;
+            double Bus = 0.09;
+            double Train = 0.06;
+
+            if (km < 20 && daytime == "day")
+            {
+                Console.WriteLine((km * taxiDay) + taxiStart);
+            }
+            else if (km < 20 && daytime == "night")
+            {
+                Console.WriteLine((km * taxiNight) + taxiStart);
+            }
+            else if (km < 100 && km >= 20)
+            {
+                double busResult = km * Bus;
+                Console.WriteLine(busResult);
+            }
+            else if (km > 100)
+            {
+                double trainResult = km * Train;
+                trainResult = Math.Round(trainResult, 2);
+                Console.WriteLine(trainResult);
+            }
+
+        }
+    }
+}
---
+++ Exercises/catTom/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broiDni = int.Parse(Console.ReadLine());
+
+            var work = 365 - broiDni;
+            var totalplaydays = work * 63 + broiDni * 127;
+            var totalMins = 30000 - totalplaydays;
+            var hours = Math.Abs(totalMins / 60);
+            var mins = Math.Abs(totalMins % 60);
+
+            if (30000 < totalplaydays)
+            {
+
+                Console.WriteLine("Tom will run away");
+                Console.WriteLine("{0} hours and {1} minutes more for play", hours, mins);
+
+            }
+            else
+            {
+                Console.WriteLine("Tom sleeps well");
+                Console.WriteLine("{0} hours and {1} minutes less for play", hours, mins);
+            }
+        }
+    }
+}
